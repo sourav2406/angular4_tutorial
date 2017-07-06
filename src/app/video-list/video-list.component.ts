cfead19edea26ccb82330225cd5dfd10c5034f53
Sourav Mandal <sourav2406@gmail.com>
2017-07-06 16:08:01 +0530

    http get included

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,33 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.css']
 })
-export class VideoListComponent implements OnInit {
+export class VideoListComponent implements OnInit, OnDestroy {
 	//videoList = ['item-1','item-2','item-3']
 	//https://www.youtube.com/embed/{{ item.embed }}
-	videoList = [
-		{
-			name:'Item 1',
-			slug:'Item-1',
-			embed:'go-kjFnZzFM'
-		},
-		{
-			name:'Item 2',
-			slug:'Item-2',
-			embed:'qqLQqGcIDs8'
-		},
-		{
-			name:'Item 3',
-			slug:'Item-3',
-			embed:'go-kjFnZzFM'
-		}
-	]
-  constructor() { }
+	private req : any;
+	videoList : [any];
+	// videoList = [
+	// 	{
+	// 		name:'Item 1',
+	// 		slug:'Item-1',
+	// 		embed:'go-kjFnZzFM'
+	// 	},
+	// 	{
+	// 		name:'Item 2',
+	// 		slug:'Item-2',
+	// 		embed:'qqLQqGcIDs8'
+	// 	},
+	// 	{
+	// 		name:'Item 3',
+	// 		slug:'Item-3',
+	// 		embed:'go-kjFnZzFM'
+	// 	}
+	// ]
+  constructor(private _http:Http) { }
 
   ngOnInit() {
+  	this.req = this._http.get('assets/json/videos.json').subscribe(data => {
+  		console.log(data.json());
+  		this.videoList = data.json() as [any];
+  	})
+  }
+
+  ngOnDestroy(){
+  	this.req.unsubscribe()
   }
 
   getEmbedUrl(item){
